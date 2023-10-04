dd08a62bbc941a46e3b71b029b5cf6c97128a107
ricardo123nac <esteban1525marti@gmail.com>
2023-10-04 18:37:08 -0500

    4 octubre

---
+++ clase_javaScript/clase1/clase.js
@@ -0,0 +1,44 @@
+alert("Holaa")
+/*var lista = new Array();
+for (var i = 0; i <2 ; i++){
+    var numero = prompt("diguite numero")
+    lista.push(numero)
+}
+document.write(lista)
+console.log(
+lista.sort( (a, b) =>{
+    return a - b
+}))
+alert(lista)*/
+/*var lista2 = []
+var x = prompt("digite numero minimo")
+var y = prompt("digite numero maximo")
+for (var i = x; i <= y; i++) { 
+
+    lista2.push(i)
+    console.log(lista2[i])
+}
+document.write(lista2)*/
+
+/*var x = prompt("Digite numero entero")
+var contador = 0
+if (x <= 0.0) {
+    for (i = 0; i <= x; i++) {
+        numero = x % 2
+        if (numero ==0) { 
+            
+        }
+    }
+} else { 
+    console.log("NUMEROS ENTEROS")
+}*/
+var acomulado =0
+var contador = 0
+while () {
+    var numeros = prompt("numeros")
+    if (numeros==0) {
+        acomulado = acomulado + numeros
+        console.log(acomulado)
+    }
+    
+}
